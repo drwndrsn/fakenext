bf2bb863000d5dcb075817c41362094fff7624b6
Drew Anderson <drw.a@outlook.com>
2017-10-01 23:27:23 -0400

    shorter

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function next (callback, req, res) {
     return function (err) {
-            if (err) callback(err)
-            else callback(null, req, res)
+            (err) ? callback(err) : callback(null, req, res)
     }
 }
 
