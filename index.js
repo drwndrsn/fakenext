cf45a7ec7878d9329db4ffa4ebdc868e366d2c37
Drew Anderson <drw.a@outlook.com>
2017-10-04 18:53:45 -0400

    changed invocation style

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-function next (callback, req, res) {
-    return function (err) {
-            (err) ? callback(err) : callback(null, req, res)
-    }
+function stub (middleware, request, response, callback) {
+
+    var next = (function (cb, req, res) {
+        return function (err) {
+                (err) ? cb(err) : cb(null, req, res)
+        }
+    })(callback, request, response)
+
+    middleware(request, response, next)
 }
 
-module.exports = next
+module.exports = stub
 
